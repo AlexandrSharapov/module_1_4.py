bdf947ec4015f002a463e4dc043cee15cdb4653a
Alexandr <crb.mail@yandex.ru>
2024-11-01 12:29:58 +0500

    organization_str_v1

---
+++ module_1_4.py
@@ -0,0 +1,16 @@
+
+my_string = input("Введите текст: ")
+print(my_string)
+# = количество символов введённого текста
+print(len(my_string))
+
+#Вывод my_string в верхнем регистре.
+print((my_string) .upper())
+#Вывод my_string в нижнем регистре.
+print((my_string) .upper() .lower())
+#удаляет все пробелы в переменной my_str.
+print((my_string) .replace(' ', ''))
+#выводит первый символ строки my_string.
+print(my_string[0])
+#Выведите последний символ строки my_string.
+print(my_string[-1])
